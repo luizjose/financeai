c4f7cf0f7c3de2c083002b662e6240063aaf53ba
luizjose <luiz.jose0597@gmail.com>
2024-11-05 18:18:08 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex w-screen items-center justify-center">
       <h1 className="text-red-500 ">Finance AI</h1>
-      <p>
-        Finance AI is a project that uses machine learning to predict stock
-        prices.
-      </p>
-      <Image
-        src="/stock-market.jpg"
-        alt="Stock Market"
-        width={500}
-        height={300}
-      />
+      <Button>Click me</Button>
     </div>
   );
 }
