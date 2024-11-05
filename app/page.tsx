d85d1f79dc61bceebb0a09d41df95bf21e07ef3b
luizjose <luiz.jose0597@gmail.com>
2024-11-05 18:05:58 -0300

    chore: add tailwindcss prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Finance AI</h1>
-      <p>Finance AI is a project that uses machine learning to predict stock prices.</p>
-      <Image src="/stock-market.jpg" alt="Stock Market" width={500} height={300} />
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="text-red-500 ">Finance AI</h1>
+      <p>
+        Finance AI is a project that uses machine learning to predict stock
+        prices.
+      </p>
+      <Image
+        src="/stock-market.jpg"
+        alt="Stock Market"
+        width={500}
+        height={300}
+      />
     </div>
   );
 }
